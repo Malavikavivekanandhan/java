36d8a56c8c8d1300cb233ce4e6e9a491266b59a9
Malavikavivekanandhan <147599570+Malavikavivekanandhan@users.noreply.github.com>
2024-09-23 03:49:33 -0700

    Add files via upload

---
+++ EmployeeDetailsName/src/org/emp/Employee.java
@@ -0,0 +1,45 @@
+package org.emp;
+
+public class Employee {
+	private void empId() {
+		System.out.println("e21ec021");
+	
+
+	}
+	private void empName() {
+		System.out.println("malavika");
+
+
+	}
+	private void empDob() {
+		System.out.println("12/04/2004");
+	
+
+	}
+	private void empPhone() {
+		System.out.println("9489693613");
+		
+
+	}
+	private void empEmail() {
+		System.out.println("malavikav25@gmail.com");
+	
+
+	}
+	private void empAddress() {
+		System.out.println("hanumantheertham");
+		
+	}
+	public static void main(String[] args) {
+		Employee E=new Employee();
+		E.empId();
+		E.empName();
+		E.empDob();
+		E.empPhone();
+		E.empEmail();
+		E.empAddress();
+		
+	}
+	
+
+}
