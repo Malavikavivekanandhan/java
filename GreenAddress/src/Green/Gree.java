e474689b9cb54e077ee5e2dbcd0841cdf5173be9
Malavikavivekanandhan <147599570+Malavikavivekanandhan@users.noreply.github.com>
2024-09-23 03:53:09 -0700

    Add files via upload

---
+++ GreenAddress/src/Green/Gree.java
@@ -0,0 +1,38 @@
+package Green;
+
+public class Gree {
+	private void greensOmr() {
+		System.out.println("road");
+		
+
+	}
+	private void greensAdayar() {
+		System.out.println("street");
+	
+
+	}
+	private void greensTambaram() {
+		System.out.println("taluk");
+	
+
+	}
+	private void greensVelacherry() {
+		System.out.println("mainroad");
+		
+
+	}
+	private void greensAnnaNagar() {
+		System.out.println("mainstreet");
+		
+
+	}
+	public static void main(String[] args) {
+		Gree G = new Gree();
+		G.greensOmr();
+		G.greensAdayar();
+		G.greensTambaram();
+		G.greensVelacherry();
+		G.greensAnnaNagar();
+	}
+
+}
